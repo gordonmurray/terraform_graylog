66ce2af1d02c6f6bea60fb28adf1b38c6fa1ff02
Gordon Murray <gordon@gordonmurray.com>
2020-08-16 00:44:12 +0100

    Updated to use Ansible provisioner

--- packer/files/apt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean -y
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # Terraform Graylog
-Use Packer to create a Graylog AMI and an Elastic AMI for use in a Graylog cluster
+
+Use Packer to create a Graylog AMI
